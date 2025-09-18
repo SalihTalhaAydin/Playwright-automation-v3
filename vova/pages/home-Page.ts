61161d9a7ccc5f92070f2d5ec5f7d37e784c35dd
vovan-u-s <oversoul6661@gmail.com>
2025-09-17 20:51:54 -0500

    done

--- vova/pages/home-Page.ts
+++ vova/pages/home-Page.ts
@@ -1,27 +1,14 @@
 import { expect, Locator, Page } from '@playwright/test'
 
 export class HomePage {
-    userName: Locator;
-    password: Locator;
-    loginButton: Locator;
+    
     cart: Locator;
-    ourMessage: Locator;
+    
     constructor(page: Page) {
-        this.userName = page.locator('input[id="user-name"]')
-        this.password = page.locator('input[id="password"]')
-        this.loginButton = page.locator('input[id="login-button"]')
+        
         this.cart = page.locator('div[id="shopping_cart_container"]')
-        this.ourMessage=page.locator('span[class="title"]')
-    }
-    async loginTest(userName: string, password: string): Promise<void> {
-        await this.userName.fill(userName)
-        await this.password.fill(password)
-        await this.loginButton.click()
-        await this.cart.click()
-    }
-    expectedMessage(titleMessage: string): void {
-      expect(this.ourMessage).toHaveText(titleMessage)
-    }
+       
+   
 }
-
+}
 
--- vova/tests/cart-test-vova.spec.ts
+++ vova/tests/cart-test-vova.spec.ts
@@ -1,11 +1,13 @@
 import { test } from '@playwright/test'
-import { HomePage } from '../pages/home-Page'
+import { LoginTestPage } from '../pages/login-page-vova'
 import { expect, Locator, Page } from '@playwright/test'
+import { CartPage } from '../pages/cart-page'
 
 test('Cart test page', async ({ page }) => {
 
-    let homePage = new HomePage(page)
+    let loginTest = new LoginTestPage(page)
     await page.goto('https://www.saucedemo.com/')
-    await homePage.loginTest('standard_user', 'secret_sauce')
-    homePage.expectedMessage('Your Cart')
+    await loginTest.loginTest('standard_user', 'secret_sauce')
+    let cartTest=new CartPage(page)
+    cartTest.expectedMessage('Your Cart')
 })
