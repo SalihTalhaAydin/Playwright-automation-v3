1b9d21762f3d30d5b83814a96b74df0ce00907a6
vovan-u-s <oversoul6661@gmail.com>
2025-09-17 21:00:32 -0500

    done

--- vova/tests/cart-test-vova.spec.ts
+++ vova/tests/cart-test-vova.spec.ts
@@ -2,12 +2,15 @@ import { test } from '@playwright/test'
 import { LoginTestPage } from '../pages/login-page-vova'
 import { expect, Locator, Page } from '@playwright/test'
 import { CartPage } from '../pages/cart-page'
+import { HomePage } from '../pages/home-Page'
 
 test('Cart test page', async ({ page }) => {
 
     let loginTest = new LoginTestPage(page)
     await page.goto('https://www.saucedemo.com/')
     await loginTest.loginTest('standard_user', 'secret_sauce')
-    let cartTest=new CartPage(page)
-    cartTest.expectedMessage('Your Cart')
+    let homePage = new HomePage(page)
+    await homePage.cart.click()
+    let cartTest = new CartPage(page)
+    await cartTest.expectedMessage('Your Cart')
 })
