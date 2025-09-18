f8522b7061e93688895a404fcff2d2b83db73334
artemN63 <nikishynartem@gmail.com>
2025-09-17 20:51:45 -0500

    Created text validation for all hamburger menu buttons

--- art/pages/home-page.ts
+++ art/pages/home-page.ts
@@ -4,14 +4,37 @@ export class HomePage {
 
     cartImageButton: Locator
     homePageTitle: Locator
+    hamburgerMenuIconButton: Locator
+    allItemsOptionButton: Locator
+    aboutOptionButton: Locator
+    logoutOptionButton: Locator
+    resetAppStateOptionButton: Locator
+
 
     constructor(page: Page) {
         this.cartImageButton = page.locator('a[data-test="shopping-cart-link"]')
         this.homePageTitle = page.locator('span[data-test="title"]')
+        this.hamburgerMenuIconButton = page.locator('button[id="react-burger-menu-btn"]')
+        this.allItemsOptionButton = page.locator('a[id="inventory_sidebar_link"]')
+        this.aboutOptionButton = page.locator('a[id="about_sidebar_link"]')
+        this.logoutOptionButton = page.locator('a[id="logout_sidebar_link"]')
+        this.resetAppStateOptionButton = page.locator('a[id="reset_sidebar_link"]')
     }
 
     titleValidation(expectedTitle: string): void {
         expect(this.homePageTitle).toHaveText(expectedTitle)
+    }
+
+    hamburgerMenuButtonTextValidation(buttonNumber: number, expectedText: string): void {
+        if(buttonNumber === 1) {
+            expect(this.allItemsOptionButton).toHaveText(expectedText)
+        } else if(buttonNumber === 2) {
+            expect(this.aboutOptionButton).toHaveText(expectedText)
+        } else if(buttonNumber === 3) {
+            expect(this.logoutOptionButton).toHaveText(expectedText)
+        } else if(buttonNumber === 4) {
+            expect(this.resetAppStateOptionButton).toHaveText(expectedText)
+        }
     }
 
 }
--- art/tests/home-page-test.spec.ts
+++ art/tests/home-page-test.spec.ts
@@ -15,3 +15,20 @@ test('Home Page title validation', async ({page}) => {
     let expectedHomePageTitle = 'Products'
     homePage.titleValidation(expectedHomePageTitle)
 })
+
+test('Home Page hamburger menu buttons text validation', async ({page}) => {
+    let loginPage = new LoginPage(page)
+    let homePage = new HomePage(page)
+
+    await loginPage.login('standard_user', 'secret_sauce')
+
+    let expectedAllItemsButton = 'All Items'
+    let expectedAboutButton = 'About'
+    let expectedLogoutButton = 'Logout'
+    let expectedResetAppState = 'Reset App State'
+
+    homePage.hamburgerMenuButtonTextValidation(1, expectedAllItemsButton)
+    homePage.hamburgerMenuButtonTextValidation(2, expectedAboutButton)
+    homePage.hamburgerMenuButtonTextValidation(3, expectedLogoutButton)
+    homePage.hamburgerMenuButtonTextValidation(4, expectedResetAppState)
+})
