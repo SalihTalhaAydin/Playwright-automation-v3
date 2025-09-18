36a76aeeec62198dedb76d375954f32cb5ff16c9
Arty-Z <arty.zhuria@gmail.com>
2025-09-17 19:50:52 -0500

    Final version

--- arty/tests/login-test.spec.ts
+++ arty/tests/login-test.spec.ts
@@ -1,11 +1,11 @@
-import {test} from '@playwright/test'
-import {LoginPage} from '../pages/login-page'
+import { test } from '@playwright/test'
+import { LoginPage } from '../pages/login-page'
 
-test('User name, password', async ({page}) =>{
+test('User name, password', async ({ page }) => {
 
-let loginPage = new LoginPage(page);
+    let loginPage = new LoginPage(page);
 
     await page.goto('https://www.saucedemo.com/');
     await loginPage.login(" ", "secret_sauce");
-    loginPage.validateErrorMessage('Epic sadface: Username is required')
+    loginPage.validateErrorMessage("Epic sadface: Username and password do not match any user in this service")
 })
