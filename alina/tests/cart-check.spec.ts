a4ff93cdace61de335324a74a2d3af71cc11e995
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-18 23:02:00 -0500

    test commit

--- alina/tests/cart-check.spec.ts
+++ alina/tests/cart-check.spec.ts
@@ -3,7 +3,8 @@ import { LoginPage } from "../pages/login-page";
 import { HomePage } from "../pages/home-page";
 import { CartPage } from "../pages/cart";
 
-test("Login, home page and cart page functionality", async ({ page }) => {
+
+test("Login1, home page and cart page functionality", async ({ page }) => {
   let loginPage = new LoginPage(page);
   let homePage = new HomePage(page);
   let cartPage = new CartPage(page);
@@ -12,6 +13,7 @@ test("Login, home page and cart page functionality", async ({ page }) => {
   await loginPage.login("standard_user", "secret_sauce");
   await homePage.verifyproductsTitle("Products");
   await homePage.verifymainTitle("Swag Labs");
+  await homePage.addToCartButton();
   await cartPage.verifyqtyHeader("QTY");
   await cartPage.verifydescriptionHeader("Description");
   await cartPage.clickContinueShopping();
